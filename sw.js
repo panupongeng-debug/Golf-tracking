98a8fd975b07271fe3dd778308bb5dfb869b2cba
panupongeng-debug <panupong.eng@gmail.com>
2026-01-06 23:53:44 +0700

    Refactor service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,6 +1,47 @@
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open('golf-pwa').then(c=>c.addAll(['./','./index.html'])));
+const CACHE_NAME = 'golf-pro-tracker-v2.1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/192/1099/1099680.png',
+  'https://cdn-icons-png.flaticon.com/512/1099/1099680.png'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
